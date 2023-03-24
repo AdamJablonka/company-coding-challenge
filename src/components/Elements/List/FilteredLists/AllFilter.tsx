60fea35ae02cf54dfcff8ad91e0652f4ac82c2ec
AdamJablonka <ajjablonka@gmail.com>
2023-03-24 01:36:58 -0400

    semi working dragging

--- src/components/Elements/List/FilteredLists/AllFilter.tsx
+++ src/components/Elements/List/FilteredLists/AllFilter.tsx
@@ -1,19 +1,32 @@
 import { useTodoStore, useSearchStore } from "../../../../stores/index";
 import { TodoMap } from "../TodoMap";
+import { useState, useEffect } from "react";
 
 export const AllFilter = () => {
   const todoList = useTodoStore((state) => state);
   const searchQuery = useSearchStore((state) => state.search);
 
-  let filteredItems: any[];
-
-  filteredItems = todoList.todos.filter((item) =>
-    item.title.toLowerCase().includes(searchQuery.toLowerCase())
+  const [filteredItems, setFilteredItems] = useState(
+    todoList.todos.filter((item) =>
+      item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
   );
+
+  useEffect(() => {
+    setFilteredItems(
+      (filteredItems) =>
+        (filteredItems = todoList.todos.filter((item) =>
+          item.title.toLowerCase().includes(searchQuery.toLowerCase())
+        ))
+    );
+  }, [searchQuery, todoList]);
 
   return (
     <>
-      <TodoMap filteredItems={filteredItems} />
+      <TodoMap
+        filteredItems={filteredItems}
+        setFilteredItems={setFilteredItems}
+      />
     </>
   );
 };
--- src/components/Elements/List/TodoMap.tsx
+++ src/components/Elements/List/TodoMap.tsx
@@ -1,12 +1,32 @@
+import { useCallback } from "react";
 import { TodoItem } from "./TodoItem";
-import { Typography } from "@mui/material";
-import { useTodoStore, useSearchStore } from "../../../stores";
+import { useTodoStore } from "../../../stores";
 import { Todo } from "../../../types";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
-export const TodoMap = ({ filteredItems }: any) => {
+export const TodoMap = ({ filteredItems, setFilteredItems }: any) => {
   const updateTodo = useTodoStore((state) => state.updateTodo);
   const deleteTodo = useTodoStore((state) => state.deleteTodo);
   const toggleComplete = useTodoStore((state) => state.toggleComplete);
+
+  const onDragEnd = useCallback(
+    (result: any) => {
+      if (!result.destination) {
+        return;
+      }
+
+      if (result.source.index === result.destination.index) {
+        return;
+      }
+
+      const newFilteredItems = Array.from(filteredItems);
+      const [removed] = newFilteredItems.splice(result.source.index, 1);
+      newFilteredItems.splice(result.destination.index, 0, removed);
+
+      setFilteredItems(newFilteredItems);
+    },
+    [filteredItems, setFilteredItems]
+  );
 
   return (
     <ul
@@ -16,15 +36,50 @@ export const TodoMap = ({ filteredItems }: any) => {
         alignItems: "center",
       }}
     >
-      {filteredItems.map((todoItem: Todo) => (
-        <TodoItem
-          key={todoItem.id}
-          todo={todoItem}
-          onUpdateTodo={updateTodo}
-          onDeleteTodo={deleteTodo}
-          onToggleComplete={toggleComplete}
-        />
-      ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
+          <Droppable droppableId="droppable-1">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                style={{
+                  width: "100%",
+                }}
+              >
+                {filteredItems.map((todoItem: Todo, index: number) => (
+                  <Draggable
+                    key={todoItem.id}
+                    draggableId={todoItem.id.toString()}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <TodoItem
+                          key={todoItem.id}
+                          todo={todoItem}
+                          onUpdateTodo={updateTodo}
+                          onDeleteTodo={deleteTodo}
+                          onToggleComplete={toggleComplete}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
     </ul>
   );
 };
